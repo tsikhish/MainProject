dc2d3fd1388fb5d5d367f42d1dab2a6d314b3599
tsikhish <tsikhishvilimariam@gmail.com>
2025-02-06 15:37:04 +0400

    Little changes made in Database

--- CasinoApi/obj/Debug/net8.0/CasinoApi.AssemblyInfo.cs
+++ CasinoApi/obj/Debug/net8.0/CasinoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CasinoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c20dd0b6938ba2c75f3c4231c4fa89bff6c7a36b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ff4cde8aaae58f529c817d5a5edfddd21eb75ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("CasinoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CasinoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
